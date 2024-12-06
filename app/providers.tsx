57f5220e6f27122c910c29a569f4405e01d22c7b
marc-antoine ourradour <ourradourm@gmail.com>
2024-12-05 20:51:40 -0500

    set up wagmi.sh

---
+++ app/providers.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { config } from "@/lib/clients/wagmish/config";
+
+export function Providers({ children }: { children: ReactNode }) {
+  const queryClient = new QueryClient();
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
